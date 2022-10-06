df2569a66fb71302ed50026eb446081f89da376e
Minh Tran <mtran8011@yahoo.com>
2022-10-05 19:33:27 -0700

    Tic Tac Toe program outline

---
+++ tictactoe.c
@@ -0,0 +1,52 @@
+// TIC TAC TOE gane between 2 players or player vs computer
+
+int main(){
+
+    // prompt user for game they wish to play
+
+    // read information from console (only 1 or 2)
+
+    /*
+    * Option 1: player v player
+    * Display board, prompt player 1 (X)
+    * read info (Format: # #; (1-3), (1-3))
+    * Display board with new X, prompt player 2 (O)
+    * read info 
+    * keep alternating between player 1 or 2 while
+    * checking if there 3 X / O in a row / col / diagonal (should be in a function)
+    */
+
+    // Display board and prompt
+
+    // Read
+
+    // Repeat . . .
+
+    // Check if there's a winner
+
+    /* 
+    * Option 2: plaver v computer
+    * Display board, prompt player (X)
+    * read info (Format: # #; (1-3), (1-3))
+    * Display board with new X, move to computer
+    * Randomly place O (should be in a function)
+    * keep alternating between player 1 or computer while
+    * checking if there's a winner
+    */
+
+    // Display board and prompt
+
+    // Read
+
+    // Repeat . . .
+
+    // Check if there's a winner
+
+
+
+    return 0;
+}
+
+// function that checks if there is 3 X's or O's in a row / col / diagonal 
+
+// function for random placement of X or O
