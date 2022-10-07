26269b0377744aa7453960a14800ed162edffdc2
Minh Tran <mtran8011@yahoo.com>
2022-10-07 15:19:53 -0700

    Implemented choice between v player / v computer

--- tictactoe.c
+++ tictactoe.c
@@ -1,10 +1,46 @@
-// TIC TAC TOE gane between 2 players or player vs computer
+// TIC TAC TOE game between 2 players or player vs computer
+#include <stdio.h>
+
+typedef enum { false, true } bool;
 
 int main(){
 
-    // prompt user for game they wish to play
+    // prompt user for game they wish to play   
+
+    int choice;
+    bool typeChoice = false;
+
+    printf( "===========================\n");
+    printf( "WELCOME TO TIC TAC TOE!\n");
+    printf( "1 --- person vs. person\n");
+    printf( "2 --- person vs. random computer\n");
+    printf( "Enter your choice (1 or 2): ");    
+
+    scanf("%d", &choice);    
+    // printf("This is the num: %d", choice);   
 
     // read information from console (only 1 or 2)
+    while(!typeChoice)
+    {
+            typeChoice = true;
+            switch (choice)
+            {
+            case 1:
+                /* code */
+                printf("v person");
+                break;
+            case 2:
+                printf("v computer");
+                break;
+            default:
+                typeChoice = false;
+                printf("===========================\n");
+                printf("Please enter the correct choice (1 or 2): ");
+                scanf("%d", &choice);
+                break;
+            }
+
+    }
 
     /*
     * Option 1: player v player
@@ -41,8 +77,6 @@ int main(){
     // Repeat . . .
 
     // Check if there's a winner
-
-
 
     return 0;
 }
